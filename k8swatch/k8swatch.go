e68029d493c950bfa017b8a30735c76e1d59ae96
David Finkel <davidf@vimeo.com>
2025-04-09 10:10:41 -0400

    k8swatch: factor out common switch body
    CreatePod and ModPod events are identical code, but operating on
    distinct structs. Pull the body out into an unexported free-function so
    we have one code-path to work with.

--- k8swatch/k8swatch.go
+++ k8swatch/k8swatch.go
@@ -8,6 +8,18 @@ import (
 	"github.com/vimeo/k8swatcher"
 )
 
+func handlePodUpdate(u *galaxycache.Universe, event k8swatcher.PodEvent, port int, isReady bool, podIP *net.IPAddr) {
+	if podIP != nil && isReady {
+		addr := net.TCPAddr{IP: podIP.IP, Port: port}
+		u.AddPeer(galaxycache.Peer{
+			ID:  event.PodName(),
+			URI: addr.String(),
+		})
+	} else {
+		u.RemovePeers(event.PodName())
+	}
+}
+
 // UpdateCB returns a callback that's appropriate for incrementally
 // updating the set of peers based on events from [k8swatcher.PodWatcher].
 // The port should be the integer port-number on which this galaxycache
@@ -16,25 +28,9 @@ func UpdateCB(u *galaxycache.Universe, port int) k8swatcher.EventCallback {
 	return func(ctx context.Context, event k8swatcher.PodEvent) {
 		switch ut := event.(type) {
 		case *k8swatcher.CreatePod:
-			if ut.IP != nil && ut.IsReady() {
-				addr := net.TCPAddr{IP: ut.IP.IP, Port: port}
-				u.AddPeer(galaxycache.Peer{
-					ID:  ut.PodName(),
-					URI: addr.String(),
-				})
-			} else {
-				u.RemovePeers(ut.PodName())
-			}
+			handlePodUpdate(u, event, port, ut.IsReady(), ut.IP)
 		case *k8swatcher.ModPod:
-			if ut.IP != nil && ut.IsReady() {
-				addr := net.TCPAddr{IP: ut.IP.IP, Port: port}
-				u.AddPeer(galaxycache.Peer{
-					ID:  ut.PodName(),
-					URI: addr.String(),
-				})
-			} else {
-				u.RemovePeers(ut.PodName())
-			}
+			handlePodUpdate(u, event, port, ut.IsReady(), ut.IP)
 		case *k8swatcher.DeletePod:
 			u.RemovePeers(ut.PodName())
 		case *k8swatcher.InitialListComplete:
