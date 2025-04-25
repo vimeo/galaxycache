2f120983f7d4c44e83647b450299beea66892fe1
David Finkel <davidf@vimeo.com>
2025-04-25 13:04:14 -0400

    consistenthash: add chtest subpackage
    Provide a way to easily construct a synthetic hash function that can
    provide keys that map to specific owning keys (hosts/peers).
    
    This will come in handy for testing the new Peek functionality.

---
+++ consistenthash/chtest/fake_hash.go
@@ -0,0 +1,163 @@
+//go:build go1.23
+
+// Package chtest provides some helper hash-functions for use with the parent
+// consistenhash package (and galaxycache) to provide particular owners for
+// specific keys.
+//
+// This package cannot be imported outside of tests. (If [testing.Testing] returns false, it will panic in init())
+package chtest
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/vimeo/galaxycache/cachekey"
+	"github.com/vimeo/galaxycache/consistenthash"
+)
+
+func init() {
+	if !testing.Testing() {
+		panic("attempt to use galaxycache's consistenthash/chtest package outside a test")
+	}
+}
+
+const singleOwnerPrefix = "Single Key Prefix\000\000"
+const fallthroughOwnerKeyPrefix = "Fallthrough Key Prefix\000\000"
+
+// SingleOwnerKey returns a key that is guaranteed to map to that owner (as
+// long as that owner was included in the call to [NewMapArgs]
+func SingleOwnerKey(owner string) string {
+	return singleOwnerPrefix + owner
+}
+
+// FallthroughKey constructs a key that is owned by original owner, and if
+// origOwner is removed from the hash-ring, falls through to fallthroughOwner.
+// ... as long as both owners were included in the call to [NewMapArgs]
+func FallthroughKey(origOwner, fallthroughOwner string) string {
+	outBuf := []byte{}
+	outBuf = append(outBuf, fallthroughOwnerKeyPrefix...)
+	outBuf = cachekey.AppendStrID(outBuf, origOwner)
+	return string(cachekey.AppendStrID(outBuf, fallthroughOwner))
+}
+
+type hashRange struct {
+	lo, hi uint32
+}
+
+// Args is an argument struct to NewMapArgs
+type Args struct {
+	Owners []string
+}
+
+// MapArgs provides the values that should be passed through to
+// [consistenthash.NewMap] (possibly indirectly)
+type MapArgs struct {
+	NSegsPerKey int
+	HashFunc    func([]byte) uint32
+}
+
+// NewMap creates a new [consistenthash.Map] with the args from the receiver.
+// It does _not_ populate the hashring.
+func (m *MapArgs) NewMap() *consistenthash.Map {
+	return consistenthash.New(m.NSegsPerKey, m.HashFunc)
+}
+
+// NewMapArgs creates a [consistenthash.Map] that uses a constructed "hash function" that is engineered to pick the correct, the integer
+// it computes required value of segsPerKey to make every ordering of
+// the entries in owners appear in the hashring.
+func NewMapArgs(args Args) MapArgs {
+	owners := args.Owners
+	ownersMap := make(map[string][]hashRange, len(owners))
+	segsPerKey := len(owners) - 1
+	nSegs := segsPerKey * len(owners)
+	type indexedString struct {
+		idx int
+		str string
+	}
+	// This doesn't need to be particularly efficient, just use a map to make sure we get the key indices right.
+	keyIdx := map[string]int{}
+	// we need both orderings, so we can't divide by two
+	ownerPairs := func(yield func(indexedString, indexedString) bool) {
+		for i, left := range owners {
+			for j, right := range owners {
+				if i == j {
+					// skip the left == right case; it's uninteresting (outside the consistenthash package itself)
+					continue
+				}
+				lIdx := keyIdx[left]
+				rIdx := keyIdx[right]
+				// Make sure to increment _after_ getting the current index.
+				keyIdx[left]++
+				keyIdx[right]++
+				l := indexedString{lIdx, left}
+				r := indexedString{rIdx, right}
+				if !yield(l, r) {
+					return
+				}
+			}
+		}
+	}
+
+	type lrPair struct {
+		l, r string
+	}
+	fallthroughs := make(map[lrPair]hashRange, nSegs)
+
+	ownerKeyHashes := make(map[string]uint32, nSegs)
+	// Use the signed max so we don't have to be exact.
+	rangeSize := uint32(math.MaxInt32 / nSegs)
+	lastUpper := uint32(0)
+	for left, right := range ownerPairs {
+		leftUpperBound := lastUpper + rangeSize
+		rightUpperBound := leftUpperBound + rangeSize
+		leftKey := strconv.Itoa(left.idx) + left.str
+		rightKey := strconv.Itoa(right.idx) + right.str
+		ownerKeyHashes[leftKey] = leftUpperBound
+		ownerKeyHashes[rightKey] = rightUpperBound
+		lRange := hashRange{lastUpper + 1, leftUpperBound}
+		ownersMap[left.str] = append(ownersMap[left.str], lRange)
+		ownersMap[right.str] = append(ownersMap[right.str], hashRange{leftUpperBound + 1, rightUpperBound})
+		fallthroughs[lrPair{left.str, right.str}] = lRange
+
+		lastUpper = rightUpperBound
+	}
+
+	h := func(buf []byte) uint32 {
+		bufStr := string(buf)
+		if hVal, ok := ownerKeyHashes[bufStr]; ok {
+			return hVal
+		}
+		if owner, ok := strings.CutPrefix(bufStr, singleOwnerPrefix); ok {
+			return ownersMap[owner][0].hi // this will panic if the owner isn't known, but that's fine
+		}
+		if owners, ok := strings.CutPrefix(bufStr, fallthroughOwnerKeyPrefix); ok {
+			origOwner, ftOwners, origOwnerErr := cachekey.ConsumeString([]byte(owners))
+			if origOwnerErr != nil {
+				panic(fmt.Errorf("invalid fallthrough key; failed to extract original owner: %w",
+					origOwnerErr))
+			}
+			ftOwner, emptyStr, fallthroughErr := cachekey.ConsumeString(ftOwners)
+			if fallthroughErr != nil {
+				panic(fmt.Errorf("invalid fallthrough key; failed to extract fallthrough owner: %w",
+					fallthroughErr))
+			}
+			// We can remove this if allowing arbitrary suffixes is useful enough
+			if len(emptyStr) != 0 {
+				panic("unconsumed component")
+			}
+			return fallthroughs[lrPair{origOwner, ftOwner}].hi
+		}
+		// TODO: Add a key pre-registration mechanism so keys that
+		// aren't entirely controlled, but can be predicted can be
+		// "directed" appropriately.
+		panic(fmt.Errorf("unknown key: %q", string(buf)))
+	}
+
+	return MapArgs{
+		NSegsPerKey: segsPerKey,
+		HashFunc:    h,
+	}
+}
