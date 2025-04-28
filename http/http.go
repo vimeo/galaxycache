5c57f1b65fe6e36403c00c31302c74234aa1f88e
David Finkel <davidf@vimeo.com>
2025-04-28 15:39:30 -0400

    http: implement Peek method & NotFoundErr handling
    Add support for propagating NotFound errors as
    well as adding support for a path that only handles peeks.
    
    Do a bit of minor modernization (e.g. ioutil.ReadAll -> io.ReadAll).
    
    Tests to come later.

--- http/http.go
+++ http/http.go
@@ -18,8 +18,9 @@ package http
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
@@ -31,6 +32,7 @@ import (
 )
 
 const defaultBasePath = "/_galaxycache/"
+const defaultPeekBasePath = "/_galaxycache_peek/"
 
 // HTTPFetchProtocol specifies HTTP specific options for HTTP-based
 // peer communication
@@ -38,17 +40,20 @@ type HTTPFetchProtocol struct {
 	// Transport optionally specifies an http.RoundTripper for the client
 	// to use when it makes a request.
 	// If nil, the client uses http.DefaultTransport.
-	transport http.RoundTripper
-	basePath  string
+	transport    http.RoundTripper
+	basePath     string
+	peekBasePath string
 }
 
-// HTTPOptions can specify the transport, base path, and stats.Recorder for
-// serving and fetching. *ONLY SPECIFY IF NOT USING THE DEFAULT "/_galaxycache/"
-// BASE PATH*.
+// HTTPOptions can specify the transport, base path, peek base path, and
+// stats.Recorder for serving and fetching.
+// *ONLY SPECIFY the base path IF NOT USING THE DEFAULT "/_galaxycache/" BASE PATH*.
+// *ONLY SPECIFY the peek base path IF NOT USING THE DEFAULT "/_galaxycache_peek/" BASE PATH*.
 type HTTPOptions struct {
-	Transport http.RoundTripper
-	BasePath  string
-	Recorder  stats.Recorder
+	Transport    http.RoundTripper
+	BasePath     string
+	PeekBasePath string
+	Recorder     stats.Recorder
 }
 
 // NewHTTPFetchProtocol creates an HTTP fetch protocol to be passed
@@ -58,7 +63,8 @@ type HTTPOptions struct {
 // HTTPHandler on the same Universe*.
 func NewHTTPFetchProtocol(opts *HTTPOptions) *HTTPFetchProtocol {
 	newProto := &HTTPFetchProtocol{
-		basePath: defaultBasePath,
+		basePath:     defaultBasePath,
+		peekBasePath: defaultPeekBasePath,
 	}
 
 	if opts == nil {
@@ -70,6 +76,9 @@ func NewHTTPFetchProtocol(opts *HTTPOptions) *HTTPFetchProtocol {
 	}
 	if opts.BasePath != "" {
 		newProto.basePath = opts.BasePath
+	}
+	if opts.PeekBasePath != "" {
+		newProto.peekBasePath = opts.PeekBasePath
 	}
 
 	return newProto
@@ -90,9 +99,10 @@ func (hp *HTTPFetchProtocol) NewFetcher(url string) (gc.RemoteFetcher, error) {
 // request; it contains a pointer to its parent Universe in order to access
 // its galaxies
 type HTTPHandler struct {
-	universe *gc.Universe
-	basePath string
-	recorder stats.Recorder
+	universe     *gc.Universe
+	basePath     string
+	peekBasePath string
+	recorder     stats.Recorder
 }
 
 // RegisterHTTPHandler sets up an HTTPHandler with a user specified path
@@ -106,28 +116,45 @@ type HTTPHandler struct {
 // if specifying a serveMux.
 func RegisterHTTPHandler(universe *gc.Universe, opts *HTTPOptions, serveMux *http.ServeMux) {
 	basePath := defaultBasePath
+	peekBasePath := defaultPeekBasePath
 	var recorder stats.Recorder
 	if opts != nil {
-		basePath = opts.BasePath
+		if opts.BasePath != "" {
+			basePath = opts.BasePath
+		}
+		if opts.PeekBasePath != "" {
+			peekBasePath = opts.PeekBasePath
+		}
 		recorder = opts.Recorder
 	}
 	newHTTPHandler := &HTTPHandler{
-		basePath: basePath,
-		universe: universe,
-		recorder: recorder,
+		basePath:     basePath,
+		peekBasePath: peekBasePath,
+		universe:     universe,
+		recorder:     recorder,
 	}
 	if serveMux == nil {
 		http.Handle(basePath, &ochttp.Handler{
 			Handler: ochttp.WithRouteTag(newHTTPHandler, basePath),
 		})
+		http.Handle(peekBasePath, &ochttp.Handler{
+			Handler: ochttp.WithRouteTag(newHTTPHandler, peekBasePath),
+		})
 	} else {
 		serveMux.Handle(basePath, ochttp.WithRouteTag(newHTTPHandler, basePath))
+		serveMux.Handle(peekBasePath, ochttp.WithRouteTag(newHTTPHandler, peekBasePath))
 	}
 }
 
 func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	peek := false
 	// Parse request.
-	if !strings.HasPrefix(r.URL.Path, h.basePath) {
+	switch {
+	case strings.HasPrefix(r.URL.Path, h.basePath):
+		peek = false
+	case strings.HasPrefix(r.URL.Path, h.peekBasePath):
+		peek = true
+	default:
 		panic("HTTPHandler serving unexpected path: " + r.URL.Path)
 	}
 	strippedPath := r.URL.Path[len(h.basePath):]
@@ -161,9 +188,11 @@ func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Fetch the value for this galaxy/key.
 	galaxy := h.universe.GetGalaxy(galaxyName)
 	if galaxy == nil {
+		w.Header().Set(galaxyPresentHeader, galaxyStatusNotFound)
 		http.Error(w, "no such galaxy: "+galaxyName, http.StatusNotFound)
 		return
 	}
+	w.Header().Set(galaxyPresentHeader, galaxyStatusFound)
 
 	ctx := r.Context()
 
@@ -174,9 +203,19 @@ func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		stats.WithMeasurements(gc.MServerRequests.M(1)),
 		stats.WithRecorder(h.recorder),
 	)
+
+	fetchMode := gc.FetchModeNoPeerBackend
+	if peek {
+		fetchMode = gc.FetchModePeek
+	}
+
 	var value gc.ByteCodec
-	err := galaxy.Get(ctx, key, &value)
+	_, err := galaxy.GetWithOptions(ctx, gc.GetOptions{FetchMode: fetchMode}, key, &value)
 	if err != nil {
+		if nfErr := gc.NotFoundErr(nil); errors.As(err, &nfErr) {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -184,20 +223,40 @@ func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(value)
 }
 
+const galaxyPresentHeader = "X-Galaxycache-Galaxy-Status"
+
+const galaxyStatusFound = "Found"
+const galaxyStatusNotFound = "Not Found"
+
 type httpFetcher struct {
-	transport http.RoundTripper
-	baseURL   string
+	transport   http.RoundTripper
+	baseURL     string
+	basePeekURL string
 }
 
 // Fetch here implements the RemoteFetcher interface for sending a GET request over HTTP to a peer
 func (h *httpFetcher) Fetch(ctx context.Context, galaxy string, key string) ([]byte, error) {
+	return h.fetch(ctx, false, galaxy, key)
+}
+
+// Peek here implements the RemoteFetcher interface for sending a GET request over HTTP to a peer
+func (h *httpFetcher) Peek(ctx context.Context, galaxy string, key string) ([]byte, error) {
+	return h.fetch(ctx, true, galaxy, key)
+}
+
+// fetch backs both Fetch and Peek
+func (h *httpFetcher) fetch(ctx context.Context, peek bool, galaxy string, key string) ([]byte, error) {
+	baseURL := h.baseURL
+	if peek {
+		baseURL = h.basePeekURL
+	}
 	u := fmt.Sprintf(
 		"%v%v/%v",
-		h.baseURL,
+		baseURL,
 		url.PathEscape(galaxy),
 		url.PathEscape(key),
 	)
-	req, err := http.NewRequest("GET", u, nil)
+	req, err := http.NewRequest(http.MethodGet, u, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -206,14 +265,29 @@ func (h *httpFetcher) Fetch(ctx context.Context, galaxy string, key string) ([]b
 		return nil, err
 	}
 	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
+	switch res.StatusCode {
+	case http.StatusNotFound:
+		switch galaxyPresent := res.Header.Get(galaxyPresentHeader); galaxyPresent {
+		case galaxyStatusNotFound:
+			return nil, errors.New("galaxy not found")
+		case galaxyStatusFound:
+			return nil, fmt.Errorf("key not found: %w", gc.TrivialNotFoundErr{})
+		default:
+			// anything else, including a missing header (either an
+			// older version of galaxycache, or the HTTP handler's
+			// not registered for that path)
+			return nil, fmt.Errorf("server returned HTTP response status code: %v; %q header: %q",
+				res.Status, galaxyPresentHeader, galaxyPresent)
+		}
+	case http.StatusOK:
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, fmt.Errorf("reading response body: %v", err)
+		}
+		return data, nil
+	default:
 		return nil, fmt.Errorf("server returned HTTP response status code: %v", res.Status)
 	}
-	data, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, fmt.Errorf("reading response body: %v", err)
-	}
-	return data, nil
 }
 
 // Close here implements the RemoteFetcher interface for closing (does nothing for HTTP)
